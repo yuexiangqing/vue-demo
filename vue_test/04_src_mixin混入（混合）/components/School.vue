2f3392a625f5dd294fb22b11fe34c08c3b0db694
yuexiaogou <xiangqing0914@icloud.com>
2022-12-01 20:00:36 +0800

    mixin混入（混合）

---
+++ vue_test/04_src_mixin混入（混合）/components/School.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <h2 @click="showName">学生姓名:{{name}}</h2>
+      <h2>学校地址:{{address}}</h2>
+    </div>
+  </template>
+  
+  <script>
+// 引入一个混合
+//   import {mixin,mixin2} from '../mixin'
+  export default {
+      name: 'SchoolIndex',
+      data(){
+          return{
+              name: '尚硅谷',
+              address: '北京'
+          }
+      },
+    //   mixins:[mixin, mixin2]
+    }
+  </script>
+  
+  <style>
+  
+  </style>
