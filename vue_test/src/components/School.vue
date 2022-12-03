5ecba8ec1b69c8ebaf02595be6d70de70e6ade0c
yuexiaogou <xiangqing0914@icloud.com>
2022-12-03 20:03:04 +0800

    TodoList 自定义事件

---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="school">
+      <h2>学校名称:{{name}}</h2>
+      <h2>学校地址:{{address}}</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: 'SchoolIndex',
+      props:['getSchoolName'],
+      data(){
+          return{
+              name: '尚硅谷',
+              address: '北京'
+          }
+      }
+    }
+  </script>
+  
+  <style scoped>
+  .school{
+    background-color: skyblue;
+    padding: 5px;
+  }
+  </style>
---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="student">
+    <h2>学生姓名:{{name}}</h2>
+    <h2>学生性别:{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'StudentIndex',
+    data(){
+        return{
+            name: '张三',
+            sex: '男',
+        }
+    }
+
+    }
+</script>
+
+<style>
+.student {
+  background-color: pink;
+  margin-top: 30px;
+}
+</style>
