2d1d80e7680bc1712084af2c50548b5c5951eb75
yuexiaogou <xiangqing0914@icloud.com>
2022-12-03 09:43:55 +0800

    TodoList_本地存储

---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="school">
+      <h2>学校名称:{{name}}</h2>
+      <h2>学校地址:{{address}}</h2>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      name: 'SchoolIndex',
+      data(){
+          return{
+              name: '尚硅谷',
+              address: '北京'
+          }
+      }
+    }
+  </script>
+  
+  <style scoped>
+  .school{
+    background-color: skyblue;
+  }
+  </style>
---
+++ vue_test/src/components/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="student">
+    <h2>学生姓名:{{name}}</h2>
+    <h2>学生性别:{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'StudentIndex',
+    data(){
+        return{
+            name: '张三',
+            sex: '男'
+        }
+    }
+}
+</script>
+
+<style>
+.student {
+  background-color: orange;
+}
+</style>
