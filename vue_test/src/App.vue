e86d5423d163646f6b8f7ff980cdac63cd6d98f7
yuexiaogou <xiangqing0914@icloud.com>
2022-12-05 18:06:03 +0800

    动画和过渡效果

---
+++ vue_test/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<test></test>
+		<test2></test2>
+	</div>
+</template>
+
+<script>
+	import Test from './components/Test'
+	import Test2 from './components/Test2'
+	
+	export default {
+		name:'App',
+		components:{Test,Test2}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ vue_test/src/components/Test2.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示 / 隐藏</button>
+    <!-- <h1 v-show="isShow" class="go">你好啊！</h1> -->
+
+    <!-- 如果给 transition 了 name属性，那么下面样式上要以name属性开头 -->
+    <transition name="hello" appear>
+        <h1 v-show="isShow">你好啊！</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestIndex',
+  data(){
+    return {
+        isShow: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1{
+    background-color: orange;
+    /* 过渡的效果，也可以写在下面 */
+    /* transition: 0.5s linear; */
+}
+/* 进入的起点 离开的终点 */
+.hello-enter,.hello-leave-to{
+  transform: translateX(-100%);
+}
+/* 进入的终点 离开的起点 */
+.hello-enter-to,.hello-leave{
+  transform: translateX(0);
+}
+.hello-enter-active,.hello-leave-active{
+  transition: 0.5s linear;
+}
+
+</style>
