3b3014b325685405d8b05efb315eed41043dab0d
yuexiaogou <xiangqing0914@icloud.com>
2022-12-06 16:43:30 +0800

    求和案例_纯Vue版

---
+++ vue_test/23_src_求和案例_纯Vue版/App.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+  <count></count>
+</div>
+</template>
+
+<script>
+import Count from './components/Count'
+export default {
+	name:'App',
+	components: {Count}
+	}
+</script>
+
+<style>
+
+</style>
