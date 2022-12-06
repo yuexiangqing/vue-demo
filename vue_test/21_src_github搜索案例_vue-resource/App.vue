523c4f41b81815d2d737cd8836e5ebcced232998
yuexiaogou <xiangqing0914@icloud.com>
2022-12-06 13:50:21 +0800

    vue-resource

---
+++ vue_test/21_src_github搜索案例_vue-resource/App.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="container">
+   <search></search>
+   <list></list>
+</div>
+</template>
+
+<script>
+import Search from './components/Search'
+import List from './components/List'
+export default {
+	name:'App',
+	components: {List,Search}
+	}
+</script>
+
+<style>
+
+</style>
