4741b8c3a64eb515918b2b6b74e7fd428e4a1282
yuexiaogou <xiangqing0914@icloud.com>
2022-12-06 20:33:54 +0800

    补充

--- vue_test/22_插槽/3_src_作用域插槽/App.vue
+++ vue_test/22_插槽/3_src_作用域插槽/App.vue
@@ -2,7 +2,7 @@
 
 <!-- 当插槽的定义在 category中，数据也在其中，但是用数据声明的结构是由插槽的使用者 App.vue来决定的 -->
 <!-- 在子组件中需要通过 slot 定义一个属性名，拿到数据 :games="games"  -->
-<!-- 在父组件中，通过 template 来写，并写上 scope 作用域 ，并给其命名，子组件传过来的数据都在scope命名的属性中 -->
+<!-- 在父组件中，通过 template 来写，并写上 scope 作用域 ，并给其命名，子组件传过来的数据都在scope命名的属性中(:games) -->
 
 
  <div class="container">
