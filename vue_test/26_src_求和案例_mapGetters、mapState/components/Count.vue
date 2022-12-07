c77add81c71faa7b10f3a3fbe2276c5a92b51428
yuexiaogou <xiangqing0914@icloud.com>
2022-12-07 14:25:26 +0800

    mapState、 mapGetters的使用

--- vue_test/26_src_求和案例_mapGetters、mapState/components/Count.vue
+++ vue_test/26_src_求和案例_mapGetters、mapState/components/Count.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <!-- <h1>当前求和为：{{he}} </h1>
+    <h3>当前求和放大10倍为:{{bigSum}} </h3>
+    <h3>我在{{xuexiao}} 学习{{xueke}}</h3> -->
+
 	<h1>当前求和为：{{sum}} </h1>
     <h3>当前求和放大10倍为:{{bigSum}} </h3>
     <h3>我在{{school}} 学习{{subject}}</h3>
@@ -27,15 +31,40 @@ export default {
 	}
   },
   computed:{
-	
+	// 上面的插值语法用的时候就可以简单点了
+	// 靠程序员自己亲自去写计算属性
+	// he(){
+	// 	return this.$store.state.sum
+	// },
+	// xuexiao(){
+	// 	return this.$store.state.school
+	// },
+	// xueke(){
+	// 	return this.$store.state.subject
+	// },
+
 	// 映射状态，借助 mapState 生成计算属性，从 state 中读取数据 ~~~~~~~（对象写法）
 	// ...mapState({he:'sum',xuexiao:'school',xueke:'subject'}),
+
+	// sum(){
+	// 	return this.$store.state.sum
+	// },
+	// school(){
+	// 	return this.$store.state.school
+	// },
+	// subject(){
+	// 	return this.$store.state.subject
+	// },
 
 	// 映射状态，借助 mapState 生成计算属性，从 state 中读取数据 ~~~~~~~（数组写法）
 	// ...mapState({sum:'sum',school:'school',subject:'subject'}),
 	...mapState(['sum','school','subject']),
 
 	// ----------------------------------------------------------------
+
+	// bigSum(){
+	// 	return this.$store.getters.bigSum
+	// }
 
 	// 借助 mapGetters 生成计算属性，从 Getters 中读取数据（对象写法）
 	// ...mapGetters({bigSum:'bigSum'})
@@ -45,18 +74,30 @@ export default {
   },
   methods:{
 	increment(){
+		// this.$store.dispatch('jia',this.n)
 		this.$store.commit('JIA',this.n)
 	},
 	decrement(){
+		// this.$store.dispatch('jian',this.n)
 		this.$store.commit('JIAN',this.n)
 	},
 
-	// ==================================================================
-	
+
+	// incrementOdd(){
+	// 	if(this.$store.state.sum % 2){
+	// 		this.$store.dispatch('jiaOdd',this.n)
+	// 	}
+	// 将代码写在index.js中
 	incrementOdd(){
 		this.$store.dispatch('jiaOdd',this.n)
 	},
 
+
+	// incrementWait(){
+	// 	setTimeout(() => {
+	// 	this.$store.dispatch('jiaWait',this.n)
+	// 	}, 500)
+	// }
 	incrementWait(){
 		this.$store.dispatch('jiaWait',this.n)
 	}
