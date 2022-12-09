76f64715900137609bbca903440c67ce058dd2cd
yuexiaogou <xiangqing0914@icloud.com>
2022-12-09 20:50:24 +0800

    缓存路由组件

---
+++ vue_test/38_src_缓存路由组件/pages/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <!-- <li>消息编号：{{$route.query.id}}</li> -->
+    <!-- <li>消息标题:{{$route.query.title}}</li> -->
+   
+    <li>消息编号：{{id}}</li>
+    <li>消息标题:{{title}}</li>
+    <!-- <li>a:{{a}}</li> -->
+    <!-- <li>a:{{b}}</li> -->
+  </ul>
+</template>
+
+<script>
+export default {
+name: 'DetailIndex',
+// props:['a','b'],
+props:['id','title']
+}
+</script>
+
+<style>
+
+</style>
