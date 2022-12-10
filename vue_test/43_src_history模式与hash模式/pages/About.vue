78be326610c47d9d39b9744287c8789ed9a63204
yuexiaogou <xiangqing0914@icloud.com>
2022-12-10 23:20:41 +0800

    history模式与hash模式

---
+++ vue_test/43_src_history模式与hash模式/pages/About.vue
@@ -0,0 +1,31 @@
+<template>
+   <h2>我是About的内容</h2>
+</template>
+
+<script>
+export default {
+name: 'AboutIndex',
+// 通过了路由规则，进入该组件时被调用
+beforeRouteEnter(to,from,next){
+   // console.log('beforeRouteEnter',to,from)
+      if(to.meta.isAuth){  //判断是否需要鉴权
+       if(localStorage.getItem('school') === 'atguigu'){
+           next()
+       } else {
+           alert('学校名不符合，无权限查看')
+       }
+      } else{
+           next()
+      }
+},
+// 通过了路由规则，离开该组件时被调用
+beforeRouteLeave(to,from,next){
+   console.log('beforeRouteEnter',to,from)
+   next()
+}
+}
+</script>
+
+<style>
+
+</style>
