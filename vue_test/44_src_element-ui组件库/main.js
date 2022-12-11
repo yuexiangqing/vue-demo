e1ffdfb229f2abdf17f18b8386c839ca9759531e
yuexiaogou <xiangqing0914@icloud.com>
2022-12-11 11:36:25 +0800

    element-ui组件库

---
+++ vue_test/44_src_element-ui组件库/main.js
@@ -0,0 +1,31 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+
+// 完整引入
+// 引入ElementUI组件库
+// import ElementUI from 'element-ui'
+// 引入ElementUI全部样式
+// import 'element-ui/lib/theme-chalk/index.css'
+// 应用 ElementUI
+// Vue.use(ElementUI)
+
+
+// 按需引入
+import { Button,Row,DatePicker} from 'element-ui'
+// 注册组件
+Vue.component('el-button', Button)
+Vue.component('el-row', Row)
+Vue.component('el-date-picker', DatePicker)
+
+
+//关闭Vue的生产提示
+Vue.config.productionTip = false
+
+
+//创建vm
+new Vue({
+	el:'#app',
+	render: h => h(App),
+})
